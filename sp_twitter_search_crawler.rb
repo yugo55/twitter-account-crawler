456628c7adb4714fa50e4c62f711902fdf81e4f0
yamauchiyugo <yugo.yamauchi@rooter.co.jp>
2024-02-18 12:05:33 +0900

    画面をスクロールさせてユーザー名を途中まで取得できるようにしました。

--- sp_twitter_search_crawler.rb
+++ sp_twitter_search_crawler.rb
@@ -17,11 +17,17 @@ sleep 1
 @chrome.send_cmd "Runtime.evaluate", expression: js
 wait_for_complete
 
-html = @chrome.send_cmd "Runtime.evaluate", expression: "document.documentElement.outerHTML"
-doc = Nokogiri.parse(html["result"]["value"])
-
 CSV.open("sp_twitter_search_crawler.csv", "w") do |csv|
-  doc.css("div[data-testid='cellInnerDiv']").each do |div|
-    puts div.css("")
+  loop do
+    sleep 1
+    html = @chrome.send_cmd "Runtime.evaluate", expression: "document.documentElement.outerHTML"
+    doc = Nokogiri.parse(html["result"]["value"])
+    doc.css("div[data-testid='cellInnerDiv']").each do |div|
+      csv << [div.at_css("span span").inner_text]
+    end
+    js = "window.scrollBy({ top: document.querySelector('div[data-testid=\"cellInnerDiv\"]:last-child').getBoundingClientRect().top, behavior: 'smooth' })"
+    sleep 1
+    @chrome.send_cmd "Runtime.evaluate", expression: js
+    break if doc.at_css("div[data-testid='cellInnerDiv']:last-child span").inner_text == "問題が発生しました。再読み込みしてください。"
   end
 end
