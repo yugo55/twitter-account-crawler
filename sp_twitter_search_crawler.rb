3a83891a690a76c07e515d0bfbe8958a59d49bac
yamauchiyugo <yugo.yamauchi@rooter.co.jp>
2024-02-19 15:37:52 +0900

    cookieを使ってログイン処理実装

--- sp_twitter_search_crawler.rb
+++ sp_twitter_search_crawler.rb
@@ -12,34 +12,44 @@ def wait_for_complete
 end
 
 @chrome = ChromeRemote.client
-search_text = gets.chomp
-js = "window.location = 'https://twitter.com/search?q=#{search_text}&src=typed_query&f=user'"
+@chrome.send_cmd "Network.enable"
+js = "window.location = 'https://twitter.com/home?lang=ja'"
+@chrome.send_cmd "Runtime.evaluate", expression: js
+cookies = JSON.parse(File.read("cookie.json"))
+# @chrome.send_cmd('Network.setCookies', cookieObject: cookies)
+@chrome.send_cmd "Network.setCookies", cookies: cookies
+
+js = "window.location = 'https://twitter.com/home?lang=ja'"
 sleep 1
 @chrome.send_cmd "Runtime.evaluate", expression: js
-wait_for_complete
 
-@chrome.send_cmd('Network.enable')
+# search_text = gets.chomp
+# js = "window.location = 'https://twitter.com/search?q=#{search_text}&src=typed_query&f=user'"
+# sleep 1
+# @chrome.send_cmd "Runtime.evaluate", expression: js
+# wait_for_complete
 
-def getResponse
-  request_id = nil
-  Timeout.timeout(30) do
-    @chrome.on('Network.responseReceived') do |res|
-      if res["response"]["url"].include?("SearchTimeline")
-        request_id = res["requestId"]
-        puts request_id
-      end
-    end
-    @chrome.listen_until { request_id }
-  end
-  puts request_id
-  @chrome.send_cmd("Network.getRequestPostData", requestId: request_id)
-end
+# @chrome.send_cmd('Network.enable')
 
-loop do
-  js = "window.scrollBy({ top: document.querySelector('div[data-testid=\"cellInnerDiv\"]:last-child').getBoundingClientRect().top, behavior: 'smooth' })"
-  sleep 1
-  @chrome.send_cmd "Runtime.evaluate", expression: js
-  wait_for_complete
-  getResponse
-  # break if doc.at_css("div[data-testid='cellInnerDiv']:last-child span")&.inner_text == "問題が発生しました。再読み込みしてください。"
-end
+# def getResponse
+#   request_id = nil
+#   Timeout.timeout(30) do
+#     @chrome.on('Network.responseReceived') do |res|
+#       if res["response"]["url"].include?("SearchTimeline")
+#         request_id = res["requestId"]
+#         puts request_id
+#       end
+#     end
+#     @chrome.listen_until { request_id }
+#   end
+#   puts request_id
+#   @chrome.send_cmd("Network.getRequestPostData", requestId: request_id)
+# end
+
+# loop do
+#   js = "window.scrollBy({ top: document.querySelector('div[data-testid=\"cellInnerDiv\"]:last-child').getBoundingClientRect().top, behavior: 'smooth' })"
+#   sleep 1
+#   @chrome.send_cmd "Runtime.evaluate", expression: js
+#   wait_for_complete
+#   # break if doc.at_css("div[data-testid='cellInnerDiv']:last-child span")&.inner_text == "問題が発生しました。再読み込みしてください。"
+# end
